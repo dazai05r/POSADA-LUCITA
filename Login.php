e6a4e99eff75b4e2cc6c669f72e94235f0c53491
EREN-25 <osamu55dazai@gmail.com>
2024-11-19 16:35:22 -0600

    FIX: correcion de los diseños

---
+++ Login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign Up</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div class="signup-container">
+    <h2 class="title">LOGIN</h2>
+    <div class="profile-section">
+      <div class="profile-photo">
+      <img src="IMAGEN/images (3).png" alt="Profile Photo">
+      </div>
+     
+    </div>
+    <form>
+    <div class="input-group">
+      <label for="username">Nombre de Usuario</label>
+      <input type="text" id="username" placeholder="Usuario">
+    </div>
+      <div class="input-group">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="********">
+      </div>
+      <button type="submit" class="signup-btn">ACCEDER</button>
+    </form>
+    
+  </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Posada Lucita</title>
+  <link rel="stylesheet" href="diseño.css">
+</head>
+<body>
+  <div class="container">
+    <!-- Logo -->
+    <div class="logo">
+      <h1>POSADA LUCITA</h1>
+    </div>
+
+    <!-- Sección principal -->
+    <div class="main-section">
+      <!-- Sección Cliente (Ahora primero) -->
+      <div class="client-section">
+        <h2>Cliente</h2>
+        <button>ACCEDER</button>
+      </div>
+
+      <!-- Sección Usuario (Ahora segundo) -->
+      <div class="user-section">
+        <h2>Usuario</h2>
+        <button>ACCEDER</button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
