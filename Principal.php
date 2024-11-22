3953a649b617427bb8407dbaa6af642e9c254e2b
EREN-25 <osamu55dazai@gmail.com>
2024-11-22 04:58:11 -0600

    FIX: Corregi el menu horizontal y agrege las opciones y el submenu

--- Principal.php
+++ Principal.php
@@ -3,11 +3,54 @@
 <head>
   <title>Responsive Sidebar</title>
   <link rel="stylesheet" href="css/menu.css">
+  <link rel="stylesheet" href="css/Horizontal.css">
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
 
 </head>
 <body>
+<header class="header" id="header">
+        <nav class="nav container">
+        <a href="#" class="nav__logo">POSADA LUCITA</a>
+            <div class="nav__menu" id="nav-menu">
+            <ul class="nav__list">
+            <li class="nav__item">
+    <a href="#" class="nav__link" id="reservaciones-link">
+        <i class="bx bx-calendar"></i> <!-- Icono para Reservaciones -->
+        <span class="link_name">Reservaciones</span>
+    </a>
+    <ul class="dropdown" id="reservaciones-dropdown">
+        <li><a href="#">Subopción 1</a></li>
+        <li><a href="#">Subopción 2</a></li>
+    </ul>
+</li>
+
+<li class="nav__item">
+    <a href="#" class="nav__link" id="registros-link">
+        <i class="bx bx-file"></i> <!-- Icono para Registros -->
+        <span class="link_name">Registros</span>
+    </a>
+    <ul class="dropdown" id="registros-dropdown">
+        <li><a href="#">Subopción 1</a></li>
+        <li><a href="#">Subopción 2</a></li>
+    </ul>
+</li>
+
+<li class="nav__item">
+    <a href="#" class="nav__link" id="usuarios-link">
+        <i class="bx bx-user"></i> <!-- Icono para Usuarios -->
+        <span class="link_name">Usuarios</span>
+    </a>
+    <ul class="dropdown" id="usuarios-dropdown">
+        <li><a href="#">Subopción 1</a></li>
+        <li><a href="#">Subopción 2</a></li>
+    </ul>
+</li>
+
+              <!-- Close button -->
+              <div class="nav__close" id="nav-close">
+                  <i class="ri-close-line"></i>
+              </div>
   <div class="sidebar">
     <div class="logo_details">
       <i class="bx bxl-audible icon"></i>
@@ -19,7 +62,6 @@
         <a href="#">
           <i class="bx bx-user"></i> <!-- Icono de usuario para CLIENTES -->
           <span class="link_name">CLIENTES</span>
-          <span class="tooltip">CLIENTES</span> <!-- Tooltip dentro del <a> -->
         </a>
       </li>
       
@@ -27,7 +69,7 @@
         <a href="#">
           <i class="bx bx-clipboard"></i> <!-- Icono para REPORTES -->
           <span class="link_name">REPORTES</span>
-          <span class="tooltip">REPORTES</span> <!-- Tooltip dentro del <a> -->
+          
         </a>
       </li>
       <!-- Más elementos del menú -->
@@ -43,9 +85,7 @@
       </li>
     </ul>
   </div>
-  <section class="home-section">
-    <div class="text">POSADA LUCITA</div>
-  </section>
+  
   <script src="JS/script.js"></script>
 </body>
 </html>
