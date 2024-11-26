f2a58eff4ed8bd982d34cd10ca3bb5afb1174bb0
EREN-25 <osamu55dazai@gmail.com>
2024-11-26 17:38:00 -0600

    feat:vista de saliadad

--- reservaciones.php
+++ reservaciones.php
@@ -260,12 +260,6 @@ include 'CRUDS/crudReservaciones.php';
       <footer>
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
       </footer>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
       <script src="js/scripts.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-      <script src="assets/demo/chart-area-demo.js"></script>
-      <script src="assets/demo/chart-bar-demo.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-      <script src="js/datatables-simple-demo.js"></script>
     </body>
 </html>
